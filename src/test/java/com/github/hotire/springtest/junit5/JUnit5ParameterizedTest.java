ddf90d375aa91de1593df2675c86514e43db1c43
user <gngh0101@gmail.com>
2020-01-09 01:11:51 +0900

    Learn how to use @MethodSource of @ParameterizedTest

--- src/test/java/com/github/hotire/springtest/junit5/JUnit5ParameterizedTest.java
+++ src/test/java/com/github/hotire/springtest/junit5/JUnit5ParameterizedTest.java
@@ -5,8 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Month;
 import java.util.EnumSet;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class JUnit5ParameterizedTest {
@@ -39,4 +43,18 @@ class JUnit5ParameterizedTest {
     assertThat(months).contains(month);
   }
 
+  @ParameterizedTest
+  @MethodSource("provideStringsForIsBlank")
+  void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input, boolean expected) {
+    assertThat(Strings.isBlank(input)).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> provideStringsForIsBlank() {
+    return Stream.of(
+      Arguments.of(null, true),
+      Arguments.of("", true),
+      Arguments.of("  ", true),
+      Arguments.of("not blank", false)
+    );
+  }
 }
