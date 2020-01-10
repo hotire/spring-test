2ec63e1c2696d68113ca3e4f1bbdf8bcdab1755a
user <gngh0101@gmail.com>
2020-01-11 01:26:11 +0900

    Learn how to use ignore of junit4

---
+++ src/test/java/com/github/hotire/springtest/junit4/IgnoreTest.java
@@ -0,0 +1,15 @@
+package com.github.hotire.springtest.junit4;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class IgnoreTest {
+
+  @Ignore
+  @Test
+  public void ignore() {
+    Assertions.fail();
+  }
+}
