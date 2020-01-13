0c70116fac5b2e759e07600c9619d22e9229ecf6
user <gngh0101@gmail.com>
2020-01-13 23:41:22 +0900

    Add example of @After / @AfterClass

---
+++ src/test/java/com/github/hotire/springtest/junit4/AfterAfterClass.java
@@ -0,0 +1,32 @@
+package com.github.hotire.springtest.junit4;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class AfterAfterClass {
+
+  private static String value;
+
+  @AfterClass
+  public static void init() {
+    assertThat(value).isNotNull();
+    assertThat(value).isEqualTo("config");
+  }
+
+  @After
+  public void config() {
+    assertThat(value).isNotNull();
+    assertThat(value).isEqualTo("test");
+    value = "config";
+  }
+
+  @Test
+  public void test() {
+    assertThat(value).isNull();
+    value = "test";
+  }
+}
