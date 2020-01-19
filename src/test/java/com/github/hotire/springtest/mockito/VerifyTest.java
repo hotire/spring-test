4c2f6b37389eb3c88ae192850df0eee2a2bcb576
user <gngh0101@gmail.com>
2020-01-19 20:16:44 +0900

    Learn how to use verify of no interaction

--- src/test/java/com/github/hotire/springtest/mockito/VerifyTest.java
+++ src/test/java/com/github/hotire/springtest/mockito/VerifyTest.java
@@ -4,6 +4,8 @@ package com.github.hotire.springtest.mockito;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 import java.util.List;
 import org.junit.Test;
@@ -26,5 +28,18 @@ public class VerifyTest {
     mocks.size();
     verify(mocks, times(1)).size();
   }
+
+  @Test
+  public void noInteraction() {
+    List mocks = mock(List.class);
+    verifyNoInteractions(mocks);
+  }
+
+  @Test
+  public void noInteractionWithSpecificMethod() {
+    List mocks = mock(List.class);
+    verify(mocks, times(0)).size();
+  }
+
 
 }
