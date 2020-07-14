24596093fcb960414896b49b53f4be739dd1741a
hotire <gngh0101@gmail.com>
2020-07-15 01:09:13 +0900

    Rename Application

--- src/main/java/com/github/hotire/springtest/Application.java
+++ src/main/java/com/github/hotire/springtest/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTestApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringTestApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
