d84111410f14e898ea1cf7f5a7987dd252f1b807
hotire <gngh0101@gmail.com>
2020-07-19 14:36:58 +0900

    Use field instead of abtaract

--- src/test/java/com/github/hotire/springtest/junit5/extention/mock/UserMockExtension.java
+++ src/test/java/com/github/hotire/springtest/junit5/extention/mock/UserMockExtension.java
@@ -1,17 +1,23 @@
 package com.github.hotire.springtest.junit5.extention.mock;
 
+import com.github.hotire.springtest.User;
+import com.github.hotire.springtest.UserService;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 
+import java.util.Arrays;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 
 public class UserMockExtension implements TestInstancePostProcessor, BeforeEachCallback {
 
     private Object testInstance;
 
     @Override
-    public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws Exception {
+    public void postProcessTestInstance( Object testInstance, ExtensionContext context) throws Exception {
         this.testInstance = testInstance;
     }
 
@@ -19,11 +25,21 @@ public class UserMockExtension implements TestInstancePostProcessor, BeforeEachC
     public void beforeEach(ExtensionContext context) throws Exception {
         context.getTestMethod().ifPresent(method -> {
             UserMock userMock =  method.getAnnotation(UserMock.class);
-            if (testInstance instanceof AbstractUserMockExtension) {
-                final AbstractUserMockExtension extension = ((AbstractUserMockExtension) testInstance);
-                when(extension.getUserService().findById(userMock.id())).thenReturn(extension.getUser());
-            }
+            Arrays.stream(testInstance.getClass().getDeclaredFields())
+                  .filter(field -> field.getType().equals(UserService.class))
+                  .findAny()
+                  .ifPresent(field -> {
+                      final User user = mock(User.class);
+                      field.setAccessible(true);
+                      try {
+                          final UserService userService = (UserService) field.get(testInstance);
+                          // when
+                          when(userService.findById(userMock.id())).thenReturn(user);
+                          when(user.getId()).thenReturn(userMock.id());
+                      } catch (IllegalAccessException ignore) {
+                          ignore.printStackTrace();
+                      }
+                  });
         });
-
     }
 }
--- src/test/java/com/github/hotire/springtest/junit5/extention/mock/UserMockExtensionTest.java
+++ src/test/java/com/github/hotire/springtest/junit5/extention/mock/UserMockExtensionTest.java
@@ -3,16 +3,23 @@ package com.github.hotire.springtest.junit5.extention.mock;
 
 import com.github.hotire.springtest.User;
 import com.github.hotire.springtest.UserController;
+import com.github.hotire.springtest.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+@ExtendWith(UserMockExtension.class)
 @SpringBootTest
-class UserMockExtensionTest extends AbstractUserMockExtension {
+class UserMockExtensionTest {
 
     @Autowired
     private UserController userController;
+
+    @MockBean
+    private UserService userService;
 
     @UserMock(id = 1L)
     @Test
