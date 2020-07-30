b640a7b42d368b38eee3d25d9e5f4cb28d44b42d
hotire <gngh0101@gmail.com>
2020-07-31 00:58:14 +0900

    Implement MyTestSuite

--- src/test/java/com/github/hotire/springtest/core/MyTestSuite.java
+++ src/test/java/com/github/hotire/springtest/core/MyTestSuite.java
@@ -1,7 +1,13 @@
 package com.github.hotire.springtest.core;
 
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import static com.github.hotire.springtest.core.CoreUtils.resultReport;
+
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -9,4 +15,10 @@ import org.junit.runners.Suite;
         MultiTest.class
 })
 public class MyTestSuite {
+    public static void main(String ... args) {
+        final JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
+        final Result result = junit.run(MyTestSuite.class);
+        resultReport(result);
+    }
 }
