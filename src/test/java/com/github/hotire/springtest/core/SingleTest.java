32d384fc5492d4148af06b747bf21b0d74781ede
hotire <gngh0101@gmail.com>
2020-08-15 11:08:18 +0900

    Add final to variable

--- src/test/java/com/github/hotire/springtest/core/SingleTest.java
+++ src/test/java/com/github/hotire/springtest/core/SingleTest.java
@@ -19,7 +19,7 @@ public class SingleTest {
     }
 
     public static void main(String ... args) {
-        JUnitCore junit = new JUnitCore();
+        final JUnitCore junit = new JUnitCore();
         junit.addListener(new TextListener(System.out));
         junit.run(SingleTest.class);
     }
