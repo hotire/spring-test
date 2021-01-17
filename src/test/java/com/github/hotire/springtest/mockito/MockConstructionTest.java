641a64a9f3bee1725e2bd58a5d20aa6d19ef302a
hotire <gngh0101@gmail.com>
2021-01-18 00:05:49 +0900

    opt

--- src/test/java/com/github/hotire/springtest/mockito/MockConstructionTest.java
+++ src/test/java/com/github/hotire/springtest/mockito/MockConstructionTest.java
@@ -1,10 +1,10 @@
 package com.github.hotire.springtest.mockito;
 
 import com.github.hotire.springtest.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class MockConstructionTest {
@@ -12,9 +12,9 @@ class MockConstructionTest {
     @Test
     void mocked() {
         try (MockedConstruction mocked = mockConstruction(User.class)) {
-            User user = new User();
+            final User user = new User();
             when(user.getName()).thenReturn("hotire");
-            Assertions.assertThat(user.getName()).isEqualTo("hotire");
+            assertThat(user.getName()).isEqualTo("hotire");
             verify(user).getName();
         }
     }
