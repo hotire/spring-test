4d611a33711034e46017f31ca73255ad144dfc50
hotire <gngh0101@gmail.com>
2022-06-02 00:41:29 +0900

    Add TestContextCore

---
+++ src/main/java/com/github/hotire/springtest/context/TestContextCore.java
@@ -0,0 +1,18 @@
+package com.github.hotire.springtest.context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.support.DefaultTestContext;
+
+/**
+ * @see org.springframework.test.context.TestContext
+ * @see DefaultTestContext
+ */
+public class TestContextCore {
+
+    /**
+     * @see DefaultTestContext#getApplicationContext()
+     */
+    public ApplicationContext getApplicationContext() {
+        return null;
+    }
+}
