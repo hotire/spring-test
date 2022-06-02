582f49cfc18daa7c03e00156c3ab1c8f1b368386
hotire <gngh0101@gmail.com>
2022-06-03 00:35:03 +0900

    Add ContextCacheCore

---
+++ src/main/java/com/github/hotire/springtest/context/cache/ContextCacheCore.java
@@ -0,0 +1,20 @@
+package com.github.hotire.springtest.context.cache;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.MergedContextConfiguration;
+import org.springframework.test.context.cache.ContextCache;
+import org.springframework.test.context.cache.DefaultContextCache;
+
+/**
+ * @see ContextCache
+ * @see DefaultContextCache
+ */
+public class ContextCacheCore {
+
+    /**
+     * @see DefaultContextCache#get(MergedContextConfiguration) 
+     */
+    public ApplicationContext get(MergedContextConfiguration key) {
+       return null;
+    }
+}
