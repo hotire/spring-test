e87cd3fbc9360d95824a673c5b6afeebdf0fcb9f
hotire <gngh0101@gmail.com>
2022-06-05 01:12:12 +0900

    Add TestContextManagerCore

---
+++ src/main/java/com/github/hotire/springtest/context/TestContextManagerCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springtest.context;
+
+import org.springframework.test.context.TestContextManager;
+
+/**
+ * @see TestContextManager
+ */
+public class TestContextManagerCore {
+}
