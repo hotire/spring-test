3ba70ec4330b0253b4cb8002ce0bd224a4dbae8d
hotire <gngh0101@gmail.com>
2022-06-05 01:12:32 +0900

    Add TestExecutionListenerCore

---
+++ src/main/java/com/github/hotire/springtest/context/TestExecutionListenerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springtest.context;
+
+/**
+ * @see org.springframework.test.context.TestExecutionListener
+ * @see org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener
+ */
+public class TestExecutionListenerCore {
+}
