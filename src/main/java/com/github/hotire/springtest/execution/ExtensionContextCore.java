c17522a4610bf8b93390c81396a5b877b42a9994
hotire <gngh0101@gmail.com>
2022-06-06 00:36:39 +0900

    Add ExtensionContextCore

---
+++ src/main/java/com/github/hotire/springtest/execution/ExtensionContextCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springtest.execution;
+
+/**
+ * @see org.junit.jupiter.api.extension.ExtensionContext
+ * @see org.junit.jupiter.engine.descriptor.ClassExtensionContext
+ */
+public class ExtensionContextCore {
+}
