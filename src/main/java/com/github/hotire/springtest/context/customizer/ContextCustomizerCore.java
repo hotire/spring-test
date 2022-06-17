62490c481d2443deb8e6515e5a0f897c3ce98826
hotire <gngh0101@gmail.com>
2022-06-17 23:39:18 +0900

    Add ContextCustomizerFactoryCore

---
+++ src/main/java/com/github/hotire/springtest/context/customizer/ContextCustomizerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springtest.context.customizer;
+
+/**
+ * @see org.springframework.test.context.ContextCustomizer
+ * @see org.springframework.boot.test.mock.mockito.MockitoContextCustomizer
+ */
+public class ContextCustomizerCore {
+}
---
+++ src/main/java/com/github/hotire/springtest/context/customizer/factory/ContextCustomizerFactoryCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springtest.context.customizer.factory;
+
+/**
+ * @see org.springframework.test.context.ContextCustomizerFactory
+ * @see org.springframework.boot.test.mock.mockito.MockitoContextCustomizerFactory
+ */
+public class ContextCustomizerFactoryCore {
+}
