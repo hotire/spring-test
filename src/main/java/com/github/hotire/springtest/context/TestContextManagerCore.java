1a3509fd7702f87b49e3bbab15b03f41b105132e
hotire <gngh0101@gmail.com>
2022-06-17 23:39:02 +0900

    Add TestContextManagerCore

--- src/main/java/com/github/hotire/springtest/context/TestContextManagerCore.java
+++ src/main/java/com/github/hotire/springtest/context/TestContextManagerCore.java
@@ -6,4 +6,8 @@ import org.springframework.test.context.TestContextManager;
  * @see TestContextManager
  */
 public class TestContextManagerCore {
+
+    TestContextManagerCore() {
+        // BootstrapUtils.resolveTestContextBootstrapper(BootstrapUtils.createBootstrapContext(testClass))
+    }
 }
